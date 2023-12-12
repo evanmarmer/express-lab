752216f9ecb34cc5949bec275e683341ba41a5bc
Evan Marmer <evanmarmer@gmail.com>
2023-12-12 16:48:06 -0700

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample';
+
 
 const app = express();
 const port = '8000';
@@ -48,5 +50,22 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html.njk', { name: name,
+    compliment: compliment
+  });
 });
+
+app.get('/game', (req, res) => {
+  if(req.query.play === 'no'){
+    res.render('goodbye.html.njk')
+  }else if(req.query.play === 'yes'){
+    res.render('game.html.njk')
+  }
+});
+
+app.get('/madlib', (req, res) => {
+  console.log("Hit madlib");
+  const {person, color, noun, adjective } = req.query
+  res.render('madlib.html.njk', {person, color, noun, adjective})
+})
